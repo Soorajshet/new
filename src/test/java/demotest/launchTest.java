37f396a861a23d9c9bfea00d1429fffc14358177
Sooraj Shet <soorajshet2000@gmail.com>
2024-10-28 14:51:21 +0530

    first commit

---
+++ src/test/java/demotest/launchTest.java
@@ -0,0 +1,18 @@
+package demotest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class launchTest {
+	@Test
+	public void sampleTest1() {
+		WebDriver drive = new ChromeDriver();
+		
+	}
+	@Test
+	public void sampleTest() {
+		System.out.println("hii");
+	}
+
+}
